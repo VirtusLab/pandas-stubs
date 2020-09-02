50ec5c3aa6432d72c50c8625c4da0cffb9a6743e
zbigniewkrolikowski <zkrolikowski@virtuslab.com>
2020-09-02 18:44:33 +0200

    snippet_tests Tests, fix frame.pyi

--- tests/snippets/test_frame.py
+++ tests/snippets/test_frame.py
@@ -1,8 +1,48 @@
+import tempfile
+
 import pandas as pd
 import numpy as np
 
 
-def test_mypy_test_init() -> None:
+def test_types_init() -> None:
     pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
     pd.DataFrame(data=[1, 2, 3, 4], dtype=np.int8)
     pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), columns=['a', 'b', 'c'], dtype=np.int8, copy=True)
+
+
+def test_types_csv() -> None:
+    df = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
+    with tempfile.TemporaryFile() as file:
+        df.to_csv()
+        df.to_csv(file.name)
+        df.to_csv(file)
+        pd.read_csv()
+        pd.read_csv(file)
+        pd.read_csv(file.name)
+
+
+def test_types_select() -> None:
+    df = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
+    df['col1']
+    df[0]
+    df[1:]
+
+
+def test_types_iloc_iat() -> None:
+    df = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
+    df.iloc[1, 1]
+    df.iloc[[1], [1]]
+    df.iat[0, 0]
+
+
+def test_types_loc() -> None:
+    df = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
+    df.loc[[0], 'col1']
+
+
+def test_types_boolean_indexing() -> None:
+    df = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
+    df[df > 1]
+    df[~(df > 1.0)]
+    # Series
+    # df[df['col1'] % 2 == 1]
