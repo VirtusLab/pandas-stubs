3a2919ea42cecbe1ecdc6b51af96a12957f88af9
zbigniewkrolikowski <zkrolikowski@virtuslab.com>
2021-04-09 17:08:14 +0200

    Fix

--- tests/snippets/test_series_str.py
+++ tests/snippets/test_series_str.py
@@ -4,16 +4,16 @@ import pandas as pd
 
 def test_types_split() -> None:
     s = pd.Series(['cat', 'dog', 'cow', 'coconut'])
-    s: pd.Series = s.str.split('o')
-    s: pd.Series = s.str.split('o', n=1)
-    s: pd.Series = s.str.split('o', expand=False)
-    s: pd.DataFrame = s.str.split('o', expand=True)
+    s2: pd.Series = s.str.split('o')
+    s3: pd.Series = s.str.split('o', n=1)
+    s4: pd.Series = s.str.split('o', expand=False)
+    s5: pd.DataFrame = s.str.split('o', expand=True)
 
 
 def test_types_replace() -> None:
     s = pd.Series(['cat', 'Dog', 'coconut'])
-    s: pd.Series = s.str.replace('do', 'ra')
-    s: pd.Series = s.str.replace('co', 'do', n=1)
-    s: pd.Series = s.str.replace('co', 'do', case=True)
-    s: pd.Series = s.str.replace(re.compile(r'(co)+'), repl='do', regex=True)
-    s: pd.Series = s.str.replace(re.compile(r'(co)+'), repl='do', flags=0)
+    s2: pd.Series = s.str.replace('do', 'ra')
+    s3: pd.Series = s.str.replace('co', 'do', n=1)
+    s4: pd.Series = s.str.replace('co', 'do', case=True)
+    s5: pd.Series = s.str.replace(re.compile(r'(co)+'), repl='do', regex=True)
+    s6: pd.Series = s.str.replace(re.compile(r'(co)+'), repl='do', flags=0)
