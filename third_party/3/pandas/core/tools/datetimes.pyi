ca999e88429755b0f9770c25e9f9a68ae398d3e0
zbigniewkrolikowski <zkrolikowski@virtuslab.com>
2021-04-13 19:39:32 +0200

    Fixed the inplace method annotations, the caveat is a slightly changed interface

--- third_party/3/pandas/core/tools/datetimes.pyi
+++ third_party/3/pandas/core/tools/datetimes.pyi
@@ -1,12 +1,12 @@
 from datetime import datetime
 from pandas._typing import ArrayLike as ArrayLike
 from pandas.core.dtypes.generic import ABCDataFrame as ABCDataFrame, ABCDatetimeIndex as ABCDatetimeIndex, ABCIndex as ABCIndex, ABCIndexClass as ABCIndexClass, ABCSeries as ABCSeries
-from typing import Any, Optional, TypeVar, Union
+from typing import Any, Optional, TypeVar, Union, List, Tuple
 
-ArrayConvertible = Union[list, tuple, ArrayLike, ABCSeries]
+ArrayConvertible = Union[List[Any], Tuple[Any], ArrayLike, ABCSeries]
 Scalar = Union[int, float, str]
 DatetimeScalar = TypeVar('DatetimeScalar', Scalar, datetime)
-DatetimeScalarOrArrayConvertible = Union[DatetimeScalar, list, tuple, ArrayLike, ABCSeries]
+DatetimeScalarOrArrayConvertible = Union[DatetimeScalar, List[Any], Tuple[Any], ArrayLike, ABCSeries]
 
 def should_cache(arg: ArrayConvertible, unique_share: float=..., check_count: Optional[int]=...) -> bool: ...
 def to_datetime(arg: Any, errors: str = ..., dayfirst: bool = ..., yearfirst: bool = ..., utc: Optional[Any] = ..., format: Optional[Any] = ..., exact: bool = ..., unit: Optional[Any] = ..., infer_datetime_format: bool = ..., origin: str = ..., cache: bool = ...) -> Any: ...
