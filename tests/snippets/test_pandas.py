befd0e37f83c96b8ac6a6af53700d34e2c39120a
joannasendorek <72493072+joannasendorek@users.noreply.github.com>
2021-09-10 16:42:24 +0200

    [Issue 76] Add json_normalize (#82)

--- tests/snippets/test_pandas.py
+++ tests/snippets/test_pandas.py
@@ -1,5 +1,5 @@
 # flake8: noqa: F841
-from typing import Union
+from typing import Any, Dict, List, Union
 
 import pandas as pd
 
@@ -46,3 +46,15 @@ def test_types_concat() -> None:
     rdf1: pd.DataFrame = pd.concat({'a': df, 'b': df2})
     rdf2: pd.DataFrame = pd.concat({1: df, 2: df2})
     rdf3: pd.DataFrame = pd.concat({1: df, None: df2})
+
+
+def test_types_json_normalize() -> None:
+    data1: List[Dict[str, Any]] = [{'id': 1, 'name': {'first': 'Coleen', 'last': 'Volk'}},
+                                  {'name': {'given': 'Mose', 'family': 'Regner'}},
+                                  {'id': 2, 'name': 'Faye Raker'}]
+    df1: pd.DataFrame = pd.json_normalize(data=data1)
+    df2: pd.DataFrame = pd.json_normalize(data=data1, max_level=0, sep=";")
+    df3: pd.DataFrame = pd.json_normalize(data=data1, meta_prefix="id", record_prefix="name", errors='raise')
+    df4: pd.DataFrame = pd.json_normalize(data=data1, record_path=None, meta='id')
+    data2: Dict[str, Any] = {'name': {'given': 'Mose', 'family': 'Regner'}}
+    df5: pd.DataFrame = pd.json_normalize(data=data2)
