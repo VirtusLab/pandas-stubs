3bd8b98ffd979e2d80786ecbcb83b82bcb41b03e
joannasendorek <jsendorek@virtuslab.com>
2021-10-21 16:27:20 +0200

    Improve assert_series_equal

--- tests/snippets/test_testing.py
+++ tests/snippets/test_testing.py
@@ -7,5 +7,6 @@ def test_types_assert_frame_equal() -> None:
     s1 = pd.Series([0, 1, 1, 0])
     s2 = pd.Series([0, 1, 1, 0])
     assert_series_equal(left=s1, right=s2)
-    assert_series_equal(s1, s2, check_freq=False, check_categorical=True, check_flags=True, check_datetimelike_compat=True)
+    assert_series_equal(s1, s2, check_freq=False, check_categorical=True, check_flags=True,
+                        check_datetimelike_compat=True)
     assert_series_equal(s1, s2, check_dtype=True, check_less_precise=True, check_names=True)
