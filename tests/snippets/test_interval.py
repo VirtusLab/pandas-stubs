e360ff6a5d2d6c8317c804a1977e7c7eb7aea939
zbigniewkrolikowski <zkrolikowski@virtuslab.com>
2022-01-03 17:46:34 +0100

    Fix

--- tests/snippets/test_interval.py
+++ tests/snippets/test_interval.py
@@ -13,15 +13,9 @@ def test_interval_arithmetic() -> None:
     i1: pd.Interval = pd.Interval(1, 2, closed='both')
     i2: pd.Interval = pd.Interval(1, right=2, closed='right')
 
-    i3: pd.Interval = i1 + i2
-    i4: pd.Interval = i1 - i2
-    i5: pd.Interval = i1 * i2
-    i6: pd.Interval = i1 / i2
-    i7: pd.Interval = i1 // i2
-
-    i3b: pd.Interval = i1 + 1
-    i4b: pd.Interval = i1 - 1
-    i5b: pd.Interval = i1 * 2
-    i6b: pd.Interval = i1 / 2
-    i7b: pd.Interval = i1 // 2
+    i3: pd.Interval = i1 + 1
+    i4: pd.Interval = i1 - 1
+    i5: pd.Interval = i1 * 2
+    i6: pd.Interval = i1 / 2
+    i7: pd.Interval = i1 // 2
 
