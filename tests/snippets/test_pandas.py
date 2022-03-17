bc5d732116a03fc4905e3e59475a959ac3115efd
Zbyszek Królikowski <63653095+zkrolikowski-vl@users.noreply.github.com>
2022-03-17 08:06:51 +0100

    Generalize type in concat, revamp test case to include variable arguments (#153)

--- tests/snippets/test_pandas.py
+++ tests/snippets/test_pandas.py
@@ -18,39 +18,70 @@ def test_types_to_datetime() -> None:
     pd.to_datetime([1, 2], unit="D", origin=3)
 
 
-def test_types_concat() -> None:
+def test_types_concat_series() -> None:
     s = pd.Series([0, 1, -10])
     s2 = pd.Series([7, -5, 10])
 
-    pd.concat([s, s2])
-    pd.concat([s, s2], axis=1)
-    pd.concat([s, s2], keys=['first', 'second'], sort=True)
-    pd.concat([s, s2], keys=['first', 'second'], names=["source", "row"])
+    # Mypy works differently for a variable and inline expression - possible unwanted behaviour
+    series_list = [s, s2]
+    result_list: pd.Series = pd.concat(series_list)
+    result_list_axis: pd.DataFrame = pd.concat(series_list, axis=1)
+    result_list_inline: pd.Series = pd.concat([s, s2])
+    result_list_inline_axis: pd.DataFrame = pd.concat([s, s2], axis=1)
 
-    # Depends on the axis
-    rs1: Union[pd.Series, pd.DataFrame] = pd.concat({'a': s, 'b': s2})
-    rs1a: Union[pd.Series, pd.DataFrame] = pd.concat({'a': s, 'b': s2}, axis=1)
-    rs2: Union[pd.Series, pd.DataFrame] = pd.concat({1: s, 2: s2})
-    rs2a: Union[pd.Series, pd.DataFrame] = pd.concat({1: s, 2: s2}, axis=1)
-    rs3: Union[pd.Series, pd.DataFrame] = pd.concat({1: s, None: s2})
-    rs3a: Union[pd.Series, pd.DataFrame] = pd.concat({1: s, None: s2}, axis=1)
+    result_list_special: pd.Series = pd.concat([s, s2], keys=['first', 'second'], sort=True)
+    result_list_special2: pd.Series = pd.concat([s, s2], keys=['first', 'second'], names=["source", "row"])
 
+    # Mypy works differently for a variable and inline expression - possible unwanted behaviour
+    # Return type depends on the axis
+    dict_str = {'a': s, 'b': s2}
+    result_dict_str: pd.Series = pd.concat(dict_str)
+    result_dict_str_axis: pd.DataFrame = pd.concat(dict_str, axis=1)
+    result_dict_str_inline: pd.Series = pd.concat({'a': s, 'b': s2})
+    result_dict_str_inline_axis: pd.DataFrame = pd.concat({'a': s, 'b': s2}, axis=1)
+    dict_int = {1: s, 2: s2}
+    result_dict_int: pd.Series = pd.concat(dict_int)
+    result_dict_int_axis: pd.DataFrame = pd.concat(dict_int, axis=1)
+    result_dict_inline: pd.Series = pd.concat({1: s, 2: s2})
+    result_dict_inline_axis: pd.DataFrame = pd.concat({1: s, 2: s2}, axis=1)
+    dict_none = {1: s, None: s2}
+    result_dict_none: pd.Series = pd.concat(dict_none)
+    result_dict_none_axis: pd.DataFrame = pd.concat(dict_none, axis=1)
+    result_dict_none_inline: pd.Series = pd.concat({1: s, None: s2})
+    result_dict_none_inline_axis: pd.DataFrame = pd.concat({1: s, None: s2}, axis=1)
+
+    result_special: pd.DataFrame = pd.concat(list(map(lambda x: s2, ["some_value", 3])), axis=1)
+
+
+def test_types_frame() -> None:
     df = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
     df2 = pd.DataFrame(data={'col1': [10, 20], 'col2': [30, 40]})
 
-    pd.concat([df, df2])
-    pd.concat([df, df2], axis=1)
-    pd.concat([df, df2], keys=['first', 'second'], sort=True)
-    pd.concat([df, df2], keys=['first', 'second'], names=["source", "row"])
+    # Mypy works differently for a variable and inline expression - possible unwanted behaviour
+    dataframe_list = [df, df2]
+    result_list: pd.DataFrame = pd.concat(dataframe_list)
+    result_list_axis: pd.DataFrame = pd.concat(dataframe_list, axis=1)
+    result_list_inline: pd.DataFrame = pd.concat([df, df2])
+    result_list_inline_axis: pd.DataFrame = pd.concat([df, df2], axis=1)
 
-    result: pd.DataFrame = pd.concat({"a": pd.DataFrame([1, 2, 3]), "b": pd.DataFrame([4, 5, 6])}, axis=1)
-    result2: Union[pd.DataFrame, pd.Series] = pd.concat({"a": pd.Series([1, 2, 3]), "b": pd.Series([4, 5, 6])}, axis=1)
+    result_list_special: pd.DataFrame = pd.concat([df, df2], keys=['first', 'second'], sort=True)
+    result_list_special2: pd.DataFrame = pd.concat([df, df2], keys=['first', 'second'], names=["source", "row"])
 
-    rdf1: pd.DataFrame = pd.concat({'a': df, 'b': df2})
-    rdf2: pd.DataFrame = pd.concat({1: df, 2: df2})
-    rdf3: pd.DataFrame = pd.concat({1: df, None: df2})
-
-    rdf4: pd.DataFrame = pd.concat(list(map(lambda x: s2, ["some_value", 3])), axis=1)
+    dict_str = {'a': df, 'b': df2}
+    result_dict_str: pd.DataFrame = pd.concat(dict_str)
+    result_dict_str_axis: pd.DataFrame = pd.concat(dict_str, axis=1)
+    result_dict_str_inline: pd.DataFrame = pd.concat({'a': df, 'b': df2})
+    result_dict_str_inline_axis: pd.DataFrame = pd.concat({'a': df, 'b': df2}, axis=1)
+    dict_int = {1: df, 2: df2}
+    result_dict_int: pd.DataFrame = pd.concat(dict_int)
+    result_dict_int_axis: pd.DataFrame = pd.concat(dict_int, axis=1)
+    result_dict_inline: pd.DataFrame = pd.concat({1: df, 2: df2})
+    result_dict_inline_axis: pd.DataFrame = pd.concat({1: df, 2: df2}, axis=1)
+    dict_none = {1: df, None: df2}
+    result_dict_none: pd.DataFrame = pd.concat(dict_none)
+    result_dict_none_axis: pd.DataFrame = pd.concat(dict_none, axis=1)
+    result_dict_none_inline: pd.DataFrame = pd.concat({1: df, None: df2})
+    result_dict_none_inline_axis: pd.DataFrame = pd.concat({1: df, None: df2}, axis=1)
 
 
 def test_types_json_normalize() -> None:
