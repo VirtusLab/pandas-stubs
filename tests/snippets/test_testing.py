42fdd31b4de2913a579df475aeb7746fbf82f8ec
Tim D. Smith <github@tim-smith.us>
2022-03-17 12:44:37 -0700

    Add parameters to assert_frame_equal (#154)
    Some parameters were added in 1.1.0.
    
    Reference: https://pandas.pydata.org/docs/reference/api/pandas.testing.assert_frame_equal.html

--- tests/snippets/test_testing.py
+++ tests/snippets/test_testing.py
@@ -1,4 +1,4 @@
-from pandas._testing import assert_series_equal
+from pandas._testing import assert_series_equal, assert_frame_equal
 
 import pandas as pd
 
@@ -10,3 +10,10 @@ def test_types_assert_series_equal() -> None:
     assert_series_equal(s1, s2, check_freq=False, check_categorical=True, check_flags=True,
                         check_datetimelike_compat=True)
     assert_series_equal(s1, s2, check_dtype=True, check_less_precise=True, check_names=True)
+
+
+def test_types_assert_frame_equal() -> None:
+    df1 = pd.DataFrame(data={'col1': [1.0, 2.0], 'col2': [3.0, 4.0]})
+    df2 = pd.DataFrame(data={'col1': [1.0, 2.0], 'col2': [3.0, 4.0]})
+    assert_frame_equal(df1, df2)
+    assert_frame_equal(df1, df2, atol=0.1, rtol=0.1)
